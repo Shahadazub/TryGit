30af159a9e000d015e4bde6dae11fb52f5801986
Rico <andgalkin@mail.ru>
2013-06-10 16:55:41 +0400

    I added some DBHelper I'm trying to make an SQLite database. Lets see, what will get.

--- TakeYourPills/src/com/shahadazub/takeyourpills/PillsActivity.java
+++ TakeYourPills/src/com/shahadazub/takeyourpills/PillsActivity.java
@@ -2,14 +2,23 @@ package com.shahadazub.takeyourpills;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.Cursor;
 import android.view.Menu;
 
 public class PillsActivity extends Activity {
+	
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_pills);
+		
+		
 	}
 
 	@Override
@@ -18,5 +27,7 @@ public class PillsActivity extends Activity {
 		getMenuInflater().inflate(R.menu.pills, menu);
 		return true;
 	}
+	
+	
 
 }
--- TakeYourPills/src/com/shahadazub/takeyourpills/PillsSettingsActivity.java
+++ TakeYourPills/src/com/shahadazub/takeyourpills/PillsSettingsActivity.java
@@ -2,14 +2,21 @@ package com.shahadazub.takeyourpills;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.view.Menu;
 
 public class PillsSettingsActivity extends Activity {
+	
+	DBHelper dbHelper;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_pills_settings);
+		
+		dbHelper = new DBHelper(this);
 	}
 
 	@Override
@@ -17,6 +24,28 @@ public class PillsSettingsActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.pills_settings, menu);
 		return true;
+	}
+	
+	class DBHelper extends SQLiteOpenHelper {
+
+		public DBHelper(Context context) {
+			super(context, "myDB", null, 1);
+		}
+		
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			db.execSQL("create table pills ("
+					+ "id integer primary key autoincrement,"
+					+ "name text"
+					+ "type integer"
+					+ "alarm integer"
+					+ ");");
+		}
+		
+		@Override
+		public void onUpgrade (SQLiteDatabase db, int oldVersion, int newVersion){
+			
+		}
 	}
 
 }
